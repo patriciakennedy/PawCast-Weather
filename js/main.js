0d7386c684bb76de869d7aa97dc51b8b670a2dc7
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2024-05-30 08:34:13 -0500

    created weather.html and css files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+function togglePage() {
+  const currentURL = window.location.href;
+  const weatherAppURL = 'weather.html';
+  const landingPageURL = 'index.html'; // Change this to the actual URL or path of the landing page
 
+  if (currentURL.includes('weather.html')) {
+    window.location.href = landingPageURL;
+  } else {
+    window.location.href = weatherAppURL;
+  }
+}
+
+// function togglePage() {
+//   const currentURL = window.location.href;
+//   const weatherAppURL = 'weather-app.html';
+//   const landingPageURL = 'landing-page.html'; // Change this to the actual URL or path of the landing page
+
+//   if (currentURL.includes('weather-app.html')) {
+//     window.location.href = landingPageURL;
+//   } else {
+//     window.location.href = weatherAppURL;
+//   }
+// }
