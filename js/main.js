ca568534fbf672d78cbbcf527255f9cc5602732f
patriciakennedy <42810547+patriciakennedy@users.noreply.github.com>
2024-06-02 11:58:19 -0500

    edited footer links

--- js/main.js
+++ js/main.js
@@ -10,17 +10,6 @@ function togglePage() {
   }
 }
 
-// function togglePage() {
-//   const currentURL = window.location.href;
-//   const weatherAppURL = 'weather-app.html';
-//   const landingPageURL = 'landing-page.html'; // Change this to the actual URL or path of the landing page
-
-//   if (currentURL.includes('weather-app.html')) {
-//     window.location.href = landingPageURL;
-//   } else {
-//     window.location.href = weatherAppURL;
-//   }
-// }
 
 
 /////////////////////////////////// weather Scripts
